ed11d5578267c086a723eecd9d4f847c220d2830
Jason Whitted <jason.whitted@gmail.com>
2020-10-08 12:02:18 -0600

    add validation

--- script.js
+++ script.js
@@ -12,12 +12,30 @@ function writeNumber() {
 
 function generateNumber() {
   var size = prompt("What is the length?", "3");
+  size = Number.parseInt(size, 10);
+  // if size is NaN then return an error
+  if (Number.isNaN(size)) {
+    alert("Invalid format.  Please enter a valid number between 3 and 10.");
+    return '';
+  }
+  if (size < 3 || size > 10) {
+    alert("Invalid length.  Please use a value between 3 and 10.");
+    return '';
+  }
   console.log('size', size, typeof size);
-  //    valid values are >=3 && <=10
 
   var isEven = prompt("Do you want this to be an even or odd number?");
+
+  if (isEven === "even") {
+    isEven = true;
+  } else if (isEven === "odd") {
+    isEven = false;
+  } else {
+    alert("Invalid number type. Please specify 'even' or 'odd'.");
+    return '';
+  }
+
   console.log('isEven', isEven, typeof isEven);
-  //   valid values are 'even' or 'odd'
 
   var isPositive = confirm("Press OK to create a positive number.\nPress Cancel to create a negative number.");
   console.log('isPositive', isPositive, typeof isPositive);
