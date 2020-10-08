21a1dc5df775e6aefb8c11988f75bbd211d3ce77
Jason Whitted <jason.whitted@gmail.com>
2020-10-08 11:46:21 -0600

    prompt for criteria

--- script.js
+++ script.js
@@ -11,6 +11,20 @@ function writeNumber() {
 }
 
 function generateNumber() {
+  var size = prompt("What is the length?", "3");
+  console.log('size', size, typeof size);
+  //    valid values are >=3 && <=10
+
+  var isEven = prompt("Do you want this to be an even or odd number?");
+  console.log('isEven', isEven, typeof isEven);
+  //   valid values are 'even' or 'odd'
+
+  var isPositive = confirm("Press OK to create a positive number.\nPress Cancel to create a negative number.");
+  console.log('isPositive', isPositive, typeof isPositive);
+  // confirm: negative or positive?
+
+  // generate a number
+
   return 0;
 }
 
